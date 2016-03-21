2ac56a9f08cd2d047e091559e5d472225f899d01
daphne van schaik <daphnevanschaik91@gmail.com>
2016-03-21 11:49:20 +0100

    responsive werkt!

--- website/js/hamburger.js
+++ website/js/hamburger.js
@@ -1,7 +1,6 @@
 /**
  * Created by Daphne on 21/03/16.
  */
-// HAMBURGLERv2
 
 function togglescroll() {
     $('body').on('touchstart', function(e) {
@@ -12,11 +11,10 @@ function togglescroll() {
 }
 
 $(document).ready(function() {
-    togglescroll()
+    togglescroll();
     $(".icon").click(function() {
         $(".mobilenav").fadeToggle(500);
         $(".top-menu").toggleClass("top-animate");
-        $("body").toggleClass("noscroll");
         $(".mid-menu").toggleClass("mid-animate");
         $(".bottom-menu").toggleClass("bottom-animate");
     });
@@ -28,7 +26,6 @@ $(document).keydown(function(e) {
     if (e.keyCode == 27) {
         $(".mobilenav").fadeOut(500);
         $(".top-menu").removeClass("top-animate");
-        $("body").removeClass("noscroll");
         $(".mid-menu").removeClass("mid-animate");
         $(".bottom-menu").removeClass("bottom-animate");
     }
