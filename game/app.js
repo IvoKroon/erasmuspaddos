4cff7ffb8b490ed6f96b8a5ec694d7e0fdcac493
Oscar Druiventak <oscar_rank_46@hotmail.com>
2016-03-23 00:01:37 +0100

    Can now save soundcloud url in database

--- game/app.js
+++ game/app.js
@@ -1,35 +1,98 @@
-var fs  = require('fs');
+// require modules
 var express = require('express');
 var path = require('path');
 var app = express();
 var bodyParser = require('body-parser');
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var request = require('request');
+var passport = require('passport');
+var SoundCloudStrategy = require('passport-soundcloud').Strategy;
 
+// set variables
+var PORT = process.env.PORT || 3000;
+var CLIENT_ID = '520afab7d67f14318b21e33ffc25f092';
+var CLIENT_SECRET = 'e883058fe4fadc535cd15c640ed1aae9';
+
+
+// require database connection
+require('./db-connection');
+
+// listen for socket connection
 io.on('connection', function (socket) {
-  // require('./socket-config.js')(socket);
+  // require('./socket-connection.js')(socket);
 });
 
+// set soundcloud strategy
+passport.use(new SoundCloudStrategy({
+    clientID: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/soundcloud/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+  	var sql = 'INSERT INTO soundcloud (access_token)VALUES('+ dbConnection.escape(accessToken) +')';
+		dbConnection.query(sql, function(err, result) {
+			if(err) return done(err);
+			return done(err, result);
+		});
+  }
+));
 
-var PORT = process.env.PORT || 3000;
-
-
+// set express middleware
+app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
-app.get('/callback', function(req, res) {
-  res.render('index');
-})
-app.get('/game', function() {
-  res.render('game');
-})
 
-app.get('*', function(req, res) {
-  res.render('index');
+
+//////////////////////////////////////
+//						ROUTES
+//////////////////////////////////////
+
+// set soundcloud authentication routs
+app.get('/auth/soundcloud', passport.authenticate('soundcloud'));
+app.get('/auth/soundcloud/callback',
+	passport.authenticate('soundcloud', { failureRedirect: '/', session: false }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
 });
 
 
+// game route
+app.get('/game', function() {
+  res.render('game');
+});
+
+app.get('/', function(req, res) {
+	// get access token from db
+  dbConnection.query('SELECT access_token from soundcloud', function(err, results) {
+  	if(err)  return res.send(err);
+	  var scToken = results.length > 0 ? results[results.length -1].access_token : null;
+	  console.log(scToken);
+  	  res.render('index', {
+  	  	SC_TOKEN: scToken,
+  	  	CLIENT_ID: CLIENT_ID
+  	  });
+  })
+
+});
+
+app.post('/saveSound', function(req, res) {
+  	
+  	var uri = req.body.uri;
+  	var created_at = req.body.created_at;
+  	if(!uri) return res.send('no uri found');
+  	var sql = 'INSERT INTO sounds (sound_url, created_at)VALUES(' + dbConnection.escape(uri) + ', ' + dbConnection.escape(created_at) +')';
+		dbConnection.query(sql, function(err, result) {
+			if(err) return res.send(err);
+				res.send('created!');
+		});
+})
+
+
+// start server
 server.listen(+PORT, function() {
   console.log('listening on port %s, open your browser on localhost:%s', PORT, PORT);
 });
--- game/public/js/recorder.js
+++ game/public/js/recorder.js
@@ -1,91 +1,100 @@
-  
-  // setup app ids
-  screencastify.setAppId('5741160238678016');
-  var CLIENT_ID = "520afab7d67f14318b21e33ffc25f092";
-  SC.initialize({
-      client_id: CLIENT_ID,
-      redirect_uri: "http://localhost:3000/callback",
+// set screencastify app id
+screencastify.setAppId('5741160238678016');
+
+// get tokens
+var CLIENT_ID = $("meta[name=SC_CLIENT_ID]").attr("content");
+var ACCESS_TOKEN = $("meta[name=SC_TOKEN]").attr("content");
+
+if(!ACCESS_TOKEN) console.warn("access token is empty");
+
+// get new recorder
+var recorder = new screencastify.Recorder();
+
+// get random sound
+function play() {
+  var audio = new Audio();
+  audio.src = './sounds/Soundfiles/Toon' + Math.ceil(Math.random() * 16) + '.mp3';
+  audio.play();
+}
+
+// start recording the browser screen
+function record() {
+  recorder.start({
+    recordConfig: { // optional
+        captureSource: 'screen', // for window picker, use 'screen' for screen picker
+        audio: {
+            mic: false,
+            system: true
+        }
+    },
+    shareUrl: 'https://your page', // URL of your page that handles shared files.
+    payload: 'optional arbitrary string' // Can be retrieved in share handler.
+  }).then(function() {
+    // START GAME HERE
+    console.log('recording')
   });
+}
+// Get file if user has stopped recording
+screencastify.onSharedFiles = function(fileIds) {
 
-  // get new recorder
-  var recorder = new screencastify.Recorder();
-  // connect to soundcluod once to get access_token
-  // SC.connect();
-  
-  // manually fill in access_token
-  // uncomment SC.connect and authenticate with soundcloud
-  // paste the access_token from the url here after authentication
-  var accessToken = '';
+  // get the file by file-id
+  screencastify.getFile(fileIds[0]).then(function(fileInfo) {
+  // create new form data and append data to it
+    var fd = new FormData();
+    fd.append('oauth_token', ACCESS_TOKEN);
+    fd.append("track[title]", 'erasmuspaddos-track1');
+    fd.append("track[asset_data]", fileInfo.file);
 
-  // get random sound
-  function play() {
-      var audio = new Audio();
-      audio.src = './sounds/Soundfiles/Toon' + Math.ceil(Math.random() * 16) + '.mp3';
-      audio.play();
-  }
-  setTimeout(function() {
-    record();
-  }, 1000);
-  // start recording the browser screen
-  function record() {
-      recorder.start({
-          recordConfig: { // optional
-              captureSource: 'screen', // for window picker, use 'screen' for screen picker
-              audio: {
-                  mic: false,
-                  system: true
-              }
-          },
-          shareUrl: 'https://your page', // URL of your page that handles shared files.
-          payload: 'optional arbitrary string' // Can be retrieved in share handler.
-      }).then(function() {
-          // recording has been started
-        console.log('recording')
-      });
-  }
-  // Get file if user has stopped recording
-  screencastify.onSharedFiles = function(fileIds) {
+    // send a ajax request to soundcloud api
+    $.ajax({
+        url: 'https://api.soundcloud.com/v1/tracks?client_id=' + CLIENT_ID,
+        type: 'POST',
+        data: fd,
+        cache: false,
+        contentType: false,
+        processData: false,
+        xhr: function() {
+            var xhr = $.ajaxSettings.xhr();
+            xhr.upload.onprogress = function(e) {
+                if (e.lengthComputable) {
+                    var percent = Math.floor((e.loaded / e.total) * 100);
+                    $("#uploadProgress").attr('value', percent);
+                    console.log(percent + '% uploaded');
+                }
+            };
+            return xhr;
+        },
+        success: function(res) {
+            console.log('Upload Complete!');
+            $("#uploadProgress").attr('value', 0);
+            saveSound(res);
+            // TODO save track data in database
+        },
+        error: function(err) {console.log(err);}
+    });
+  });
+  // return true so that screencastify know that we are handling the file
+  return true;
+}
 
-      // get the file by file-id
-      screencastify.getFile(fileIds[0]).then(function(fileInfo) {
-        // create new form data and append data to it
-          var fd = new FormData();
-          fd.append('oauth_token', accessToken);
-          fd.append("track[title]", 'erasmuspaddos-track1');
-          fd.append("track[asset_data]", fileInfo.file);
+// stop recording
+function stop() {
+    recorder.stop();
+}
 
-          // send a ajax request to soundcloud api
-          $.ajax({
-              url: 'https://api.soundcloud.com/v1/tracks?client_id=' + CLIENT_ID,
-              type: 'POST',
-              data: fd,
-              cache: false,
-              contentType: false,
-              processData: false,
-              xhr: function() {
-                  var xhr = $.ajaxSettings.xhr();
-                  xhr.upload.onprogress = function(e) {
-                      if (e.lengthComputable) {
-                          var percent = Math.floor((e.loaded / e.total) * 100);
-                          $("#uploadProgress").attr('value', percent);
-                          console.log(percent + '% uploaded');
-                      }
-                  };
-                  return xhr;
-              },
-              success: function(res) {
-                  console.log('Upload Complete!');
-                  console.dir(res); // This is the JSON object of the resulting track
-                  $("#uploadProgress").attr('value', 0);
-                  // TODO save track data in database
-              },
-              error: function(err) {console.log(err);}
-          });
-      });
-      // return true so that screencastify know that we are handling the file
-      return true;
-  }
-  // stop recorder form recording
-  function stop() {
-      recorder.stop();
-  }
+// save sound url to database
+function saveSound(data) {
+  console.log(data);
+  $.ajax({
+    type: 'POST',
+    url: '/saveSound',
+    data: data,
+    success: function(res) {
+      console.log(res);
+      console.log('saved in database!');
+    },
+    error: function(err) {
+      console.log('saving in database failed!');
+    }
+  })
+}
